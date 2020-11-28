53fe054ecd613fda7bf6f362ed0794fc58a2544b
anaiszed447@gmail.com <anaiszed447@gmail.com>
2020-11-28 00:28:02 -0500

    Created the UI pages for Activity 1,2 , rows and ViewRecords.

---
+++ app/src/main/java/com/example/project_jones_zulfequar/ViewRecords.kt
@@ -0,0 +1,11 @@
+package com.example.project_jones_zulfequar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRecords : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_records)
+    }
+}
