13853acf1dd1caf1f5b82590711ff9f2e3caa19d
anaiszed447@gmail.com <anaiszed447@gmail.com>
2020-11-28 23:21:06 -0500

    In the MainActivity.kt I wrote down instructions to check is the username and password were entered correctly. If they were, url one will passed into getData which will be sent to the MyViewModel then display toast message "Valid". If the username or password was false. It would do the same function but toast message would display "Invalid."

--- app/src/main/java/com/example/project_jones_zulfequar/MainActivity.kt
+++ app/src/main/java/com/example/project_jones_zulfequar/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.project_jones_zulfequar
 
+import android.accessibilityservice.GestureDescription
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit  var vm : MyViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        vm = ViewModelProvider(this,  ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(MyViewModel :: class.java)
+        loginButton.setOnClickListener {
+            var url = "http://mohameom.dev.fast.sheridanc.on.ca/login/verify.php?name=user&password=12345"
+            var url2 = "http://mohameom.dev.fast.sheridanc.on.ca/login/verify.php"
+            if(userNameText.text.isNotBlank() || passwordText.text.isNotBlank()) {
+                var username = userNameText.text.toString();
+                var password = passwordText.text.toString();
 
-        var url = "http://mohameom.dev.fast.sheridanc.on.ca/login/verify.php?name=user&password=12345"
-        var url2 = "http://mohameom.dev.fast.sheridanc.on.ca/login/verify.php"
+                if(username == "" || password == ""){
+                    userNameText.text.clear()
+                    passwordText.text.clear()
+                } else if (username == "admin" && password == "admin" || username == "user" && password == "12345"){
+                        getData(url)
+                    val i = Intent(this, MainActivity2::class.java)
+                    startActivity(i)
+                }else{
+                    getData(url2)
+                }
+
+            }else{
+                Toast.makeText(this, "Please fill in every field", Toast.LENGTH_SHORT).show()
+            }
 
 
+
+        }
 
     }
     fun getData(url : String) {
-
+            vm.makeRequest(url)
     }
 }
