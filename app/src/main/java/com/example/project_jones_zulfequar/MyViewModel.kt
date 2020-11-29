5cea4eecefa3f92a2070cef1d537c1b80f511386
anaiszed447@gmail.com <anaiszed447@gmail.com>
2020-11-28 21:43:55 -0500

    Created 3 functions called loadData, makeRequest and getJson in the MyViewModel.kt

--- app/src/main/java/com/example/project_jones_zulfequar/MyViewModel.kt
+++ app/src/main/java/com/example/project_jones_zulfequar/MyViewModel.kt
@@ -1,4 +1,21 @@
 package com.example.project_jones_zulfequar
 
-class MyViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import org.json.JSONObject
+
+class MyViewModel(application: Application) : AndroidViewModel(application) {
+
+    fun loadData(urlString: String){
+
+    }
+
+    fun makeRequest(url: String){
+
+    }
+
+    fun getJason(jsonStr:String) {
+
+    }
+
 }
