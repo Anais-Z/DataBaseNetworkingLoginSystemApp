75c29a8a9cc974480229c44269176861ef997a93
anaiszed447@gmail.com <anaiszed447@gmail.com>
2020-11-28 21:31:32 -0500

    Created 2 url variables and created the getData function which will take the url data to the MyViewModel class.

--- app/src/main/java/com/example/project_jones_zulfequar/MainActivity.kt
+++ app/src/main/java/com/example/project_jones_zulfequar/MainActivity.kt
@@ -8,9 +8,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //this is a comment for Alex. Alex please reply
-        //Hi this is Alex
-        //k i think it's fine
-        //im so tired
+        var url = "http://mohameom.dev.fast.sheridanc.on.ca/login/verify.php?name=user&password=12345"
+        var url2 = "http://mohameom.dev.fast.sheridanc.on.ca/login/verify.php"
+
+
+
+    }
+    fun getData(url : String) {
+
     }
 }
