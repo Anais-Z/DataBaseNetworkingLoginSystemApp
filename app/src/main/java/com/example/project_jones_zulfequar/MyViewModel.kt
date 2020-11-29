5bd6bce49d0208eec39bab4424eb7f52eeff0575
anaiszed447@gmail.com <anaiszed447@gmail.com>
2020-11-28 22:25:31 -0500

    In the loadData function. I created a variable called conn which will connect with the url. I then requested the method GET which will help retrieve data to the server. I also created a variable ins of type InputStream which will input the server's data in to the app. I also created a string called result which is used to return the inputted information to the app.

--- app/src/main/java/com/example/project_jones_zulfequar/MyViewModel.kt
+++ app/src/main/java/com/example/project_jones_zulfequar/MyViewModel.kt
@@ -1,16 +1,40 @@
 package com.example.project_jones_zulfequar
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MyViewModel(application: Application) : AndroidViewModel(application) {
 
-    fun loadData(urlString: String){
+    fun loadData(urlString: String): String {
 
+        var ins : InputStream? = null
+        var result = ""
+        try {
+            var url = URL(urlString)
+            val conn = url.openConnection() as HttpURLConnection
+            conn.requestMethod = "GET"
+            val response = conn.responseCode
+            Log.d("response code", "${response}")
+            ins = conn.inputStream
+
+            result = ins.bufferedReader().use (BufferedReader :: readText)
+
+            Log.d("result", result)
+        }catch (e : Exception){
+            Log.d("TAG", e.toString())
+        }
+        return result
     }
 
     fun makeRequest(url: String){
+
 
     }
 
