48d0dde2630ac5de9e0da4380a4b25cdb80f88eb
alexjones <alexjones202@outlook.com>
2020-12-07 04:42:41 -0500

    Started working on MainActivity3

--- app/src/main/java/com/example/project_jones_zulfequar/MainActivity3.kt
+++ app/src/main/java/com/example/project_jones_zulfequar/MainActivity3.kt
@@ -1,11 +1,40 @@
 package com.example.project_jones_zulfequar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main3.*
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        //Change tvDisplayID to show the id that was clicked on in ViewRecords
+
+        var newScore = etScore.text.toString()
+        var newComment = etComments.text.toString()
+
+        btnUpdate.setOnClickListener{
+            //obtain the two inputs that the user typed in the two textboxes
+            if (newScore == "" || newComment == ""){
+                Toast.makeText(this, "Please fill in every field", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //change them in the database
+
+            }
+
+
+
+        }
+
+        btnCancel.setOnClickListener{
+            //go back to viewRecords
+            val i = Intent(this, ViewRecords::class.java)
+            startActivity(i)
+
+        }
     }
 }
